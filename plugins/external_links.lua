25e6a8c049dbc19ac752dda5995929695a1e10f1
Thomas Letan <lthms@soap.coffee>
2020-02-16 17:08:59 +0100

    Add a dedicated icon for GitHub links

--- plugins/external_links.lua
+++ plugins/external_links.lua
@@ -6,8 +6,11 @@ while index do
   href = HTML.get_attribute(link, "href")
 
   if href then
-    if Regex.match(href, "^https?://") then
-      icon = HTML.parse("<i class=\"fa fa-external-link\" aria-hidden=\"true\"></i>")
+    if Regex.match(href, "^https?://github.com") then
+      icon = HTML.parse("<i class=\"url-mark fa fa-github\" aria-hidden=\"true\"></i>")
+      HTML.append_child(link, icon)
+    elseif Regex.match(href, "^https?://") then
+      icon = HTML.parse("<i class=\"url-mark fa fa-external-link\" aria-hidden=\"true\"></i>")
       HTML.append_child(link, icon)
     end
   end
