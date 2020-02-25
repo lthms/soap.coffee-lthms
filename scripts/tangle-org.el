767aa5b69fdb8459075b3f7b04addee3c7a282a8
Thomas Letan <lthms@soap.coffee>
2020-02-25 09:32:46 +0100

    Allow cleopatra to create missing directories

--- scripts/tangle-org.el
+++ scripts/tangle-org.el
@@ -2,6 +2,8 @@
 (cd (getenv "ROOT"))
 (setq org-confirm-babel-evaluate nil)
 (setq org-src-preserve-indentation t)
+(add-to-list 'org-babel-default-header-args
+             '(:mkdirp . "yes"))
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((shell . t)))
