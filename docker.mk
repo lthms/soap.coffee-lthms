0d270e913c55451b23ec3c03c0704b9641302fdd
Thomas Letan <lthms@soap.coffee>
2026-01-02 14:50:31 +0100

    Rely on variables for docker naming

--- docker.mk
+++ docker.mk
@@ -1,3 +1,5 @@
+REGISTRY := "ams.vultrcr.com/lthms"
+IMAGE := "www/soap.coffee"
 
 .PHONY: build-docker
 build-docker:
@@ -5,10 +7,16 @@ build-docker:
 	docker build \
 		-f ./build.Dockerfile \
 		--target soap.coffee \
-		-t www/soap.coffee:latest \
+		-t $(IMAGE):latest \
 		.
 
+.PHONY: push
+push:
+	docker tag $(IMAGE):latest $(REGISTRY)/$(IMAGE):latest
+	docker push $(REGISTRY)/$(IMAGE):latest
+
+.PHONY: static
 static: build-docker
-	docker create --name soap-coffee-build www/soap.coffee:latest
+	docker create --name soap-coffee-build $(IMAGE):latest
 	docker cp soap-coffee-build:/bin/soap.coffee .
 	docker rm -f soap-coffee-build
